d3fa805f7ff0b2e3b7ba5a117d9bc4932b7f2b2d
Arpan NLN <arpannln@ucla.edu>
2018-06-14 12:16:34 -0700

    display basically done

--- React Calculator/caculator/src/App.js
+++ React Calculator/caculator/src/App.js
@@ -3,16 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = { userInput: "", calculation: "" };
+  }
+  
   render() {
+    const numbers = [0, 1,'AC',2, 3, 4, 5, 6, 7, 8, 9, '='];
+    const operators = ['x', '/', '+', '-'];
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="calculator">
+        <div className="calculation">{this.state.calculation}</div>
+        <div className="numbers">
+          {numbers.map( (number) => {
+            return (<div className="number"> {number} </div>)
+          })}
+        </div>
+        <div className="operators">
+        </div>
       </div>
     );
   }
