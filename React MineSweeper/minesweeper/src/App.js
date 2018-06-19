1261a724167a414bdacbf3e679e6c927698d47b9
Arpan NLN <arpannln@ucla.edu>
2018-06-18 17:29:59 -0700

    randomize logic

--- React MineSweeper/minesweeper/src/App.js
+++ React MineSweeper/minesweeper/src/App.js
@@ -9,13 +9,32 @@ class App extends Component {
   }
   
   componentDidMount() {
-    
+    this.randomBoard(5);
   }
   //plan: grid represented by buttons 
   // need to randomize n * 3 bombs
   // need everything else to count amount of bombs around it 
   randomBoard(n) {
-    
+    let board = new Array(n)
+    for (let i = 0; i < board.length; i++) {
+      board[i] = new Array(n);
+    }
+    this.randomizeBombs(board, 5)
+    console.log(board);
+  }
+  
+  randomizeBombs(board, bombCount) {
+    console.log(bombCount);
+    if (bombCount === 0) {
+      return;
+    }
+    let [x, y] = [Math.floor(Math.random() * board.length), Math.floor(Math.random() * board.length)];
+    if (board[x][y] === '*') {
+      this.randomizeBombs(board, bombCount); 
+    } else {
+      board[x][y] = '*';
+      this.randomizeBombs(board, bombCount--);
+    }
   }
   
   render() {
