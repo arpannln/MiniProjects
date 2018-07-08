e82734a844dba409da1c9142418e26d3c545f1b6
Arpan NLN <arpannln@ucla.edu>
2018-07-07 18:25:18 -0700

    way to handle custom returns

--- JQueryLite/jquery.js
+++ JQueryLite/jquery.js
@@ -1,3 +1,39 @@
+// const getInfoFromURL = path => {
+//    const URL = require(“url”).URL;
+//    const myUrl = new URL(path)
+//    const pathname = myUrl.pathname
+//
+//    const getUsernameFromURL = pathname => {
+//       const regex = new RegExp(‘/@’);
+//       const username = pathname.split(regex).slice(1)[0]
+//       if(!username) {
+//         return “Error in parsing: URL needs to be in format://hostname:port/@username”
+//       }
+//       return username
+//     }
+//
+//     const getPathnameFromURL = pathname => {
+//        const regex = new RegExp(‘/’);
+//        const name = pathname.split(regex).slice(1)[0]
+//        if(!name) {
+//           return “Error in parsing: URL needs to be in format://hostname:port/pathname”
+//        }
+//       return name
+//      }
+//
+//     return (param) => {
+//        if (param === “username”) return getUsernameFromURL(pathname)
+//        else if (param === “pathname”) return getPathnameFromURL(pathname)
+//        else return “error”
+//     }
+// }
+//
+// // You should get “xiaoyunyang”
+// getInfoFromURL(“https://medium.com/@xiaoyunyang")("username")
+//
+// // You should get “@xiaoyunyang
+// getInfoFromURL(path)(“pathname”)
+
 (function() {
   if (typeof $ === "undefined") {
     window.$ = {};
